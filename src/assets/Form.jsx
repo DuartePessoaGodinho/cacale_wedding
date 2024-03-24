dd2c5f9692023e13040efb458ff513652d791b9b
Duarte Godinho <duarte.r.godinho@gmail.com>
2024-03-24 17:52:06 +0000

    form

--- src/assets/Form.jsx
+++ src/assets/Form.jsx
@@ -90,6 +90,21 @@ const ContactForm = () => {
           </Button>
         </Form>
       </Container>
+      <form name="contact" netlify>
+        <p>
+          <label>
+            Name <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Email <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </>
   );
 };
